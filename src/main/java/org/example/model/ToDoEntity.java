30e16152dfd80689c3f5c180fe089793c369fbfc
Blaire <113817141+hh-blaire-cho@users.noreply.github.com>
2025-12-25 20:16:43 +0900

    컬럼명 명시 지우기

--- src/main/java/org/example/model/ToDoEntity.java
+++ src/main/java/org/example/model/ToDoEntity.java
@@ -25,7 +25,7 @@ public class ToDoEntity {
     @Column(nullable = false)
     private String title;
 
-    @Column(nullable = false, name="toDoOrder")
+    @Column(nullable = false)
     private Long order;
 
     @Column(nullable = false)
